3147cafec159790c57e63ec2d5a511f2dd371d8f
evolesor <evolesorsan@gmail.com>
2026-03-11 02:46:26 -0700

    Initial commit

---
+++ src/views/Contact.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div class="container">
+        <div class="alert alert-light" role="alert">
+        A simple light alert—check it out!
+        </div>
+
+        <h1>Show Contact</h1>
+        <h3>Khwannetr Taswut</h3>
+        <p>Welcome to vue</p>
+    </div>
+
+    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+h3{
+    color: #025aac;
+}
+    
+</style>
